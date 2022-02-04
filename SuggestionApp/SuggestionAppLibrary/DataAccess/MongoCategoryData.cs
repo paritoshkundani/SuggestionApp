4099c612f147de6a5480670127a6de32653a35ac
Paritosh Kundani <paritosh.kundani@prutech.com>
2022-02-04 08:56:05 -0500

    Lesson 10

---
+++ SuggestionApp/SuggestionAppLibrary/DataAccess/MongoCategoryData.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace SuggestionAppLibrary.DataAccess;
+
+public class MongoCategoryData : ICategoryData
+{
+    private readonly IMongoCollection<CategoryModel> _categories;
+    private readonly IMemoryCache _cache;
+    private const string cacheName = "CategoryData";
+
+    public MongoCategoryData(IDbConnection db, IMemoryCache cache)
+    {
+        _cache = cache;
+        _categories = db.CategoryCollection;
+    }
+
+    /*
+     * TC mentions another approach called Lazy Caching, did not show it. Basically if multiple calls come here and
+     * nothing is cached yet, all would call db and cache the same info, lazy cache apporach would make the requests
+     * queue up, so only the first go to db and add to cache then others would be able to try and there would be cache
+     * already, incase the call is expensive that is another approach.
+     */
+    public async Task<List<CategoryModel>> GetAllCategories()
+    {
+        var output = _cache.Get<List<CategoryModel>>(cacheName);
+
+        if (output is null)
+        {
+            var results = await _categories.FindAsync(_ => true);
+            output = results.ToList();
+
+            _cache.Set(cacheName, output, TimeSpan.FromDays(1));
+        }
+
+        return output;
+    }
+
+    public Task CreateCategory(CategoryModel category)
+    {
+        return _categories.InsertOneAsync(category);
+    }
+}
---
+++ SuggestionApp/SuggestionAppLibrary/DataAccess/MongoUserData.cs
@@ -0,0 +1,45 @@
+﻿namespace SuggestionAppLibrary.DataAccess;
+
+public class MongoUserData : IUserData
+{
+    private readonly IMongoCollection<UserModel> _users;
+
+    public MongoUserData(IDbConnection db)
+    {
+        _users = db.UserCollection;
+    }
+
+    // get all user records
+    public async Task<List<UserModel>> GetUsersAsync()
+    {
+        var results = await _users.FindAsync(_ => true);
+        return results.ToList();
+    }
+
+    // get one user
+    public async Task<UserModel> GetUser(string id)
+    {
+        var results = await _users.FindAsync(u => u.Id == id);
+        return results.FirstOrDefault();
+    }
+
+    // locate based on Azure B2C object identifier
+    public async Task<UserModel> GetUserFromAuthentication(string objectId)
+    {
+        var results = await _users.FindAsync(u => u.ObjectIdentifier == objectId);
+        return results.FirstOrDefault();
+    }
+
+    public Task CreateUser(UserModel user)
+    {
+        return _users.InsertOneAsync(user);
+    }
+
+    public Task UpdateUser(UserModel user)
+    {
+        // locate the user based on Id
+        var filter = Builders<UserModel>.Filter.Eq("id", user.Id);
+        // replace the one is db with what we passed in, if not there add a new one to db (IsUpsert = true)
+        return _users.ReplaceOneAsync(filter, user, new ReplaceOptions { IsUpsert = true });
+    }
+}
