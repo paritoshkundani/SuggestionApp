eaeaba88ca2688f0821143917e03002d67875251
Paritosh Kundani <paritosh.kundani@prutech.com>
2022-02-12 07:01:55 -0500

    Lesson 16

---
+++ SuggestionApp/SuggestionAppUI/Components/MyInputRadioGroup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace SuggestionAppUI.Components;
+
+// TC did this as he said there was some issue requiring him to double click the radio button to register the selection.
+// After he typed some data into an input field and then tried to select a radio button it was not registering the 
+// selection without him having to click twice.
+public class MyInputRadioGroup<TValue> : InputRadioGroup<TValue>
+{
+    private string _name;
+    private string _fieldClass;
+
+    protected override void OnParametersSet()
+    {
+        var fieldClass = EditContext?.FieldCssClass(FieldIdentifier) ?? string.Empty;
+        if (fieldClass != _fieldClass || Name != _name)
+        {
+            _fieldClass = fieldClass;
+            _name = Name;
+            base.OnParametersSet();
+        }
+    }
+}
---
+++ SuggestionApp/SuggestionAppUI/Models/CreateSuggestionModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuggestionAppUI.Models;
+
+public class CreateSuggestionModel
+{
+    [Required]
+    [MaxLength(75)]
+    public string Suggestion { get; set; }
+    
+    [Required]
+    [MinLength(1)]
+    [Display(Name = "Category")]
+    // added min length so something must be there, otherwise a blank string would be allowed with just Required
+    public string CategoryId { get; set; }
+    
+    [MaxLength(500)]
+    public string Description { get; set; }
+}
